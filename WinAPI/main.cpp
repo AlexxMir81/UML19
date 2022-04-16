43a77d737df5f20c0830403186a4aa1b8808d781
Miro_up10@itstepru.onmicrosoft.com <Miro_up10@itstepru.onmicrosoft.com>
2022-04-16 16:17:42 +0300

    correct

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -2,6 +2,7 @@
 
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, int nCmdShow)
 {
+	MessageBox(NULL, "Hello World!", "Info", MB_OK | MB_ICONINFORMATION);
 
 	return 0;
 }
