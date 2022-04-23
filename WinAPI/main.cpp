e2a658b56d54f2a6266de45f5bc9c56820d602f6
Miro_up10@itstepru.onmicrosoft.com <Miro_up10@itstepru.onmicrosoft.com>
2022-04-23 12:30:14 +0300

    homework

--- WinAPI/main.cpp
+++ WinAPI/main.cpp
@@ -1,6 +1,10 @@
-﻿#include<winapifamily.h>
+﻿//#include<winapifamily.h>
 #include<Windows.h>
 #include"resource.h"
+
+CONST CHAR g_szLoginDefaulMessage[] = "Введите логин";
+CONST CHAR g_szPasswordDefaulMessage[] = "Введите пароль";
+
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM LParam);
 
@@ -21,15 +25,58 @@ BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM LParam)
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		HWND hLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		HWND hPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+		SendMessage(hLogin, WM_SETTEXT, 0, (LPARAM)g_szLoginDefaulMessage);
+		SendMessage(hPassword, WM_SETTEXT, 0, (LPARAM)g_szPasswordDefaulMessage);
 	}
 		break;
 	case WM_COMMAND: //обрабатывает нажатие кнопок и любые обращения к элементам окна.
+	{
+		CONST INT SIZE = 256;
+		CHAR sz_buffer[SIZE] = {};
+		HWND hLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+		HWND hPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+
 		switch (LOWORD(wParam))
 		{
 		case IDOK: MessageBox(hwnd, "Была нажата кнопка OK", "Info", MB_OK | MB_ICONINFORMATION); break;
 		case IDCANCEL: EndDialog(hwnd, 0); break;
+			/////////////////////////
+		case IDC_BUTTON_COPY:
+		{
+			//CONST INT SIZE = 256;
+			//CHAR sz_buffer[SIZE] = {};
+			//HWND hLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
+			//HWND hPassword = GetDlgItem(hwnd, IDC_EDIT_PASSWORD);
+			SendMessage(hLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+			SendMessage(hPassword, WM_SETTEXT, 0, (LPARAM)sz_buffer);
 		}
 		break;
+		////////////////////////
+		case IDC_EDIT_LOGIN:
+			if (HIWORD(wParam) == EN_SETFOCUS)
+			{
+				/*CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE] = {};
+				HWND hLoggin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);*/
+				SendMessage(hLogin, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				if (strcmp(sz_buffer, g_szLoginDefaulMessage) == 0)
+					SendMessage(hLogin, WM_SETTEXT, 0, (LPARAM)"");
+			}
+			else if (HIWORD(wParam) == EN_KILLFOCUS)
+			{
+				/*CONST INT SIZE = 256;
+				CHAR sz_buffer[SIZE] = {};
+				HWND hPassword = GetDlgItem(hwnd, IDC_EDIT_LOGIN);*/
+				SendMessage(hPassword, WM_GETTEXT, SIZE, (LPARAM)sz_buffer);
+				if (strlen(sz_buffer) == 0)
+					SendMessage(hPassword, WM_SETTEXT, 0, (LPARAM)g_szLoginDefaulMessage);
+			}
+			break;
+		}
+		break;
+	}
 	case WM_CLOSE: //Обрабатывает нажатие на крестик.
 		EndDialog(hwnd, 0);
 	}
